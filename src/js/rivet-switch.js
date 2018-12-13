e7141bd60613ab8a2843a8d50be71524ac2c4907
lmcgrana <lmcgrana@iu.edu>
2018-12-13 14:33:48 -0500

    Updates copyright banners

--- src/js/rivet-switch.js
+++ src/js/rivet-switch.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) [year of first publication] The Trustees of Indiana University
+ * Copyright (C) 2018 The Trustees of Indiana University
  * SPDX-License-Identifier: BSD-3-Clause
  */
 (function (root, factory) {
