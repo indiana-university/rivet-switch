6e813a8fa241e088d4a299596c3b9b7522581336
Rachel <racmocon@gmail.com>
2019-02-27 10:09:23 -0500

    Adjusted linting and updated .gitignore file.

--- cypress/integration/switch_spec.js
+++ cypress/integration/switch_spec.js
@@ -7,6 +7,7 @@ const DEV_SERVER = "http://localhost:3000";
 describe("Rivet switch interactions", function() {
   it("Click the default Rivet switch", function() {
     cy.visit(DEV_SERVER);
+
     cy.get(SWITCHABLE_BUTTON)
       .click()
       .should("have.attr", "aria-checked", "false");
@@ -28,8 +29,10 @@ describe("Rivet switch interactions", function() {
 describe("Switch API methods", function() {
   it("Call the .off() method", function() {
     cy.visit(DEV_SERVER);
+
     cy.window().then(win => {
       const toggleSwitch = win.document.querySelector(SWITCHABLE_BUTTON);
+
       win.Switch.off(toggleSwitch);
     });
 
@@ -43,6 +46,7 @@ describe("Switch API methods", function() {
   it("Call the .on() method", function() {
     cy.window().then(win => {
       const toggleSwitch = win.document.querySelector(SWITCHABLE_BUTTON);
+
       win.Switch.on(toggleSwitch);
     });
 
@@ -57,13 +61,18 @@ describe("Switch API methods", function() {
 describe("Listen for custom events", function() {
   it("Listen for the switchOn name", function() {
     cy.visit(DEV_SERVER);
+
     cy.window().then(win => {
       cy.spy(console, "log");
+
       win.addEventListener(win.Switch.on.name, function() {
         console.log(win.Switch.on.name);
       });
+
       const toggleSwitch = win.document.querySelector(LISTENER_BUTTON);
+
       win.Switch.on(toggleSwitch);
+
       expect(console.log).to.be.called;
     });
   });
@@ -71,11 +80,15 @@ describe("Listen for custom events", function() {
   it("Listen for the switchOff name", function() {
     cy.window().then(win => {
       cy.spy(console, "log");
+
       win.addEventListener(win.Switch.off.name, function() {
         console.log(win.Switch.off.name);
       });
+
       const toggleSwitch = win.document.querySelector(LISTENER_BUTTON);
+
       win.Switch.off(toggleSwitch);
+
       expect(console.log).to.be.called;
     });
   });
