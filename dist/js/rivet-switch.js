cd52ab1b9aee4e096a2d2555f73ea057b8b0b5db
iubot <iubot@iu.edu>
2019-05-29 15:13:27 +0000

    Circle CI: update package.json version. [skip ci]

--- dist/js/rivet-switch.js
+++ dist/js/rivet-switch.js
@@ -3,7 +3,7 @@
  * Copyright (C) 2018 The Trustees of Indiana University
  * SPDX-License-Identifier: BSD-3-Clause
 
- * rivet-switch - @version 0.3.0
+ * rivet-switch - @version 0.3.1-rc.25
  */
 
 /**
